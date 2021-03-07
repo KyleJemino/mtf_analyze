31753d718974c9e3d397a6ccd91d380b03cb2adb
Kyle <kyljmn@kyljmn.localdomain>
2021-03-07 14:59:09 +0800

    Adds onClick to image mapper

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -6,11 +6,16 @@ import { StateContext } from '../App.js'
 const ImageContainer = () => {
   const context = useContext(StateContext)
 
+  const onClick = (e) => {
+    console.log("x: ", e.nativeEvent.offsetX)
+    console.log("y: ", e.nativeEvent.offsetY)
+  }
+
   return (
     <>
       <div className="image-section">
         { context.state.image !== "" 
-          ? <ImageMapper src={context.state.image}/>
+          ? <ImageMapper src={context.state.image} onImageClick={(e) => onClick(e)} />
           : <p>No Image Uploaded yet</p>
         }
       </div>
